e7ba3252f21bbb22dafa5473b4798c0b8c30b482
Ordecaos <jackwatkinson1996@hotmail.co.uk>
2021-11-04 15:27:30 +0000

    Updates app.py, homepage and personalinformation to work in the VM

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, SelectField, SubmitField
+import pymysql
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('db_uri')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = getenv('secretkey')
 
